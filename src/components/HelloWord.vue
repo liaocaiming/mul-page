da03d8138fec59b72a0aa9a543cc9001041c4ae1
liaocaiming <1963182458@qq.com>
2019-02-13 19:19:56 +0800

    vue: 格式化文件
    Signed-off-by: liaocaiming <1963182458@qq.com>

--- src/components/HelloWord.vue
+++ src/components/HelloWord.vue
@@ -7,28 +7,27 @@
 </template>
 
 <script lang="ts">
-
 import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class Hello extends Vue {
-    enthusiasm = 1;
-    data () {
-        return {
-            name: 'liaocaiming'
-        }
+  enthusiasm = 1;
+  data() {
+    return {
+      name: "liaocaiming"
+    };
+  }
+  increment() {
+    this.enthusiasm++;
+  }
+  decrement() {
+    if (this.enthusiasm > 1) {
+      this.enthusiasm--;
     }
-    increment() {
-        this.enthusiasm++;
-    }
-    decrement() {
-        if (this.enthusiasm > 1) {
-            this.enthusiasm--;
-        }
-    }
+  }
 
-    get exclamationMarks(): string {
-        return Array(this.enthusiasm + 1).join('!');
-    }
+  get exclamationMarks(): string {
+    return Array(this.enthusiasm + 1).join("!");
+  }
 }
 </script>
